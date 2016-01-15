1c933dde52146865a8a084d37a249c67d1ebad74
Gilbertson, John <jgilbert@liverpool.ac.uk>
2016-01-15 15:17:36 +0000

    FEATURE: passthrough file with first filter options.

--- index.js
+++ index.js
@@ -24,6 +24,42 @@ const port=opts.port||8080;
 
 function handler(req,resp) {
   const inUrl=url.parse(req.url,true);
+  const file=inUrl.pathname;
+  const param=inUrl.query;
+  https.get("https://www.liverpool.ac.uk"+file,res=>{
+    console.log(res.headers);
+    if(!file.match(/\.json$/) && !res.headers["content-type"].match(/json/g)){
+      resp.end("JSON files only");
+    } else {
+      var data="";
+      res.on('data', (chunk) => {
+        data+=chunk.toString();
+      });
+      res.on('end', () => {
+        if(param.combined)
+        {
+          data=filters.combined(data);
+        }
+        if(param.unescape) {
+          data=filters.unescape(data);
+        }
+        if(param.removeTrailingcomma) {
+          data=filters.removeTrailingcomma(data);
+        }
+        if(param.arrayify) {
+          data=JSON.stringify(filters.arrayify(data));
+        }
+        if(param.removeEmpty) {
+          data=JSON.stringify(filters.removeEmpty(JSON.parse(data)));
+        }
+        resp.setHeader("Content-type","application/json");
+        resp.end(data);
+      });
+
+      //res.pipe(resp);
+    }
+  });
+  //resp.end(JSON.stringify(inUrl));
 }
 
 if(opts.key && opts.cert){
